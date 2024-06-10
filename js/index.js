d0f5766d5a7009abed07dc87788cf637f30e161a
csy10001 <fuitshine@gmail.com>
2024-06-11 05:36:48 +0900

    main timer 추가
    -main 타이머 추가했습니다.
    -main 약간의 버그 수정했습니다.

--- js/index.js
+++ js/index.js
@@ -2,8 +2,16 @@ const 정답 = "APPLE";
 
 let attempts = 0;
 let index = 0;
+let timer;
 
 function appStart() {
+  const displayGameover = () => {
+    const div = document.createElement("div");
+    div.innerText = "게임이 종료됐습니다.";
+    div.style =
+      "display:flex; justify-content:center; align-tiems: center; position:absolute; top:40vh; left:45vw; background-color:white;";
+    document.body.appendChild(div);
+  };
   const nextLine = () => {
     if (attempts === 6) return;
     attempts += 1;
@@ -12,6 +20,8 @@ function appStart() {
 
   const gameover = () => {
     window.removeEventListener("keydown", handleKeydown);
+    displayGameover();
+    clearInterval(timer);
   };
 
   const handleEnterKey = () => {
@@ -34,13 +44,42 @@ function appStart() {
     if (맞은_갯수 === 5) gameover();
     else nextLine();
   };
+
+  const handleBackspace = () => {
+    if (index > 0) {
+      const preBlock = document.querySelector(
+        `.board-column[data-index='${attempts}${index - 1}']`
+      );
+      preBlock.innerText = "";
+    }
+    if (index !== 0) index -= 1;
+  };
+
+  const startTimer = () => {
+    const 시작_시간 = new Date();
+
+    function setTime() {
+      const 현재_시간 = new Date();
+      const 흐른_시간 = new Date(현재_시간 - 시작_시간);
+      const 분 = 흐른_시간.getMinutes().toString();
+      const 초 = 흐른_시간.getSeconds().toString();
+      const timeDiv = document.querySelector("#timer");
+      timeDiv.innerText = `${분}:${초}`;
+    }
+
+    timer = setInterval(setTime, 1000); //1초마다 함수 / 주기성
+    //setTimeout(sayHello); // 한번 함수
+  };
+
   const handleKeydown = (event) => {
     const key = event.key.toUpperCase();
     const keyCode = event.keyCode;
     const thisBlock = document.querySelector(
       `.board-column[data-index='${attempts}${index}']`
     );
-    if (index === 5) {
+
+    if (event.key === "Backspace") handleBackspace();
+    else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
       else return;
     } else if (65 <= keyCode && keyCode <= 90) {
@@ -49,6 +88,7 @@ function appStart() {
     }
   };
 
+  startTimer();
   window.addEventListener("keydown", handleKeydown);
 }
 
